02597134e30a241bc02e75195efb9ef7bc039511
Rahul Pratap Singh <ace.rahulthakur@gmail.com>
2026-03-04 06:52:00 +0530

    Change Cookie Options

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const cookieOptions = {
   httpOnly: true,
-  secure: true,                    
-  sameSite: "none", 
+  secure: isProd,                    
+  sameSite: isProd ? "none" : "lax", 
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
